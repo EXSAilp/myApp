bac1927d3ac4a87029e404117bd3dac63bbec344
Satayoo Maneekul <satayoo.m@ku.th>
2022-06-27 12:34:13 +0700

    Update Cat and add Dog

--- src/myapp/dog.java
+++ src/myapp/dog.java
@@ -1,7 +1,7 @@
 package myapp;
 
-public class cat {
+public class dog {
     public static void main(String[] args) {
-        System.out.println("Cat");
+        System.out.println("Dog");
     }
 }
